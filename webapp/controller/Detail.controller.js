ed8e1e84d233778b3990c3df24515e97629d4703
ricardoRodiani <ricardo.rodiani@gmail.com>
2021-02-22 12:00:12 -0300

    Finished step 34

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,6 +1,11 @@
 sap.ui.define(
-    ["sap/ui/core/mvc/Controller", "sap/ui/core/UIComponent", "sap/ui/core/routing/History"],
-    function (Controller, UIComponent, History) {
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/core/UIComponent",
+        "sap/ui/core/routing/History",
+        "sap/m/MessageToast",
+    ],
+    function (Controller, UIComponent, History, MessageToast) {
         "use strict";
         return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
             onInit: function () {
@@ -10,6 +15,7 @@ sap.ui.define(
                     .attachPatternMatched(this._onObjectMatched, this);
             },
             _onObjectMatched: function (oEvent) {
+                this.byId("rating").reset();
                 this.getView().bindElement({
                     path:
                         "/" +
@@ -20,16 +26,26 @@ sap.ui.define(
                 });
             },
             onNavBack: function () {
-                var oHistory = History.getInstance()
-                var sPreviousHash = oHistory.getPreviousHash()
+                var oHistory = History.getInstance();
+                var sPreviousHash = oHistory.getPreviousHash();
 
                 if (sPreviousHash !== undefined) {
-                    window.history.go(-1)
+                    window.history.go(-1);
                 } else {
-                    var oRouter = UIComponent.getRouterFor(this)
-                    oRouter.navTo("overview", {}, true)
+                    var oRouter = UIComponent.getRouterFor(this);
+                    oRouter.navTo("overview", {}, true);
                 }
-            }
+            },
+            onRatingChange: function (oEvent) {
+                var fValue = oEvent.getParameter("value");
+                var oResourceBundle = this.getView()
+                    .getModel("i18n")
+                    .getResourceBundle();
+
+                MessageToast.show(
+                    oResourceBundle.getText("ratingConfirmation", [fValue])
+                );
+            },
         });
     }
 );
