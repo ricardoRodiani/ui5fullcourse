04b26d96a15bc8444ba2ff913dd022df2e2590f7
ricardoRodiani <ricardo.rodiani@gmail.com>
2021-02-22 10:35:47 -0300

    Finished step 32

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define(
-    ["sap/ui/core/mvc/Controller", "sap/ui/core/UIComponent"],
-    function (Controller, UIComponent) {
+    ["sap/ui/core/mvc/Controller", "sap/ui/core/UIComponent", "sap/ui/core/routing/History"],
+    function (Controller, UIComponent, History) {
         "use strict";
         return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
             onInit: function () {
@@ -19,6 +19,17 @@ sap.ui.define(
                     model: "invoice",
                 });
             },
+            onNavBack: function () {
+                var oHistory = History.getInstance()
+                var sPreviousHash = oHistory.getPreviousHash()
+
+                if (sPreviousHash !== undefined) {
+                    window.history.go(-1)
+                } else {
+                    var oRouter = UIComponent.getRouterFor(this)
+                    oRouter.navTo("overview", {}, true)
+                }
+            }
         });
     }
 );
